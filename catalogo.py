3103d3942b348a2447c0a78c5d010bfeaf7ac15d
kpersich0054 <44851328+kpersich0054@users.noreply.github.com>
2026-04-27 19:02:27 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -244,7 +244,7 @@ for row_group in rows:
                     unsafe_allow_html=True
                 )
                 qtd = st.number_input(
-                    "qtd",
+                    "",
                     min_value=1,
                     max_value=int(row["estoque_atual"]),
                     value=1,
@@ -256,7 +256,7 @@ for row_group in rows:
                     unsafe_allow_html=True
                 )
                 st.number_input(
-                    "qtd",
+                    "",
                     min_value=0,
                     max_value=0,
                     value=0,
