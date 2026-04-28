8eea374c165137f046b1334cb8b8106d260ba92b
kpersich0054 <44851328+kpersich0054@users.noreply.github.com>
2026-04-28 16:12:11 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -99,6 +99,17 @@ header[data-testid="stHeader"] {{
     display: none;
 }}
 
+/* remove espaço que sobra em cima */
+.block-container {{
+    padding-top: 0rem !important;
+}}
+
+/* remove botão lateral (>>) */
+button[kind="header"] {{
+    display: none;
+}}
+
+
 /* =========================
    CORREÇÃO BACKGROUND (SEM QUEBRAR SIDEBAR)
 ========================= */
