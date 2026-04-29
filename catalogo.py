d56f650947c6864b5bd936e9cf6affda748f67d0
kpersich0054 <44851328+kpersich0054@users.noreply.github.com>
2026-04-29 10:34:26 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -144,7 +144,7 @@ st.markdown(f"""
     2px -2px 0 #A6E8FB, -2px -2px 0 #A6E8FB,
 
     /* sombra suave (profundidade) */
-    0 2px 8px rgba(0,0,0,.45);
+    0 1px 4px rgba(0,0,0,.45);
 
 }}
 </style>
