27a972a42c0143f8b1f6b6486f067910bf0bfd04
kpersich0054 <44851328+kpersich0054@users.noreply.github.com>
2026-04-30 09:12:31 -0300

    Update catalogo.py

--- catalogo.py
+++ catalogo.py
@@ -86,15 +86,14 @@ config = get_config()
 st.markdown(f"""
 <style>
 .modarte-hero {{
-  position: sticky;
+  position: fixed;
   top: 0;
   left: 0;
+  right: 50%;
+  margin-left: -50vw;
+  margin-right: -50vw;
   width: 100vw;
   z-index: 999;
-}}
-
-.block-container {{
-  padding-top: 260px !important;
 }}
 
 .modarte-hero img {{
